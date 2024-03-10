ea38a34e38f71e581ee189149610068930fd438c
Artem Khachaturov <43856383+ez4papiz1@users.noreply.github.com>
2024-03-10 14:36:06 -0700

    Added a user model file User.model.js

---
+++ DiscordCode/BackEnd/routes/Models/User.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const User = mongoose.model('user', UserSchema)
+module.exports = User
