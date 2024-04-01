2d290df7d50340e3e8a70e14c9d7a67f74862f1e
Artem Khachaturov <43856383+ez4papiz1@users.noreply.github.com>
2024-03-31 17:54:48 -0700

    Store user info in session

--- DiscordCode/BackEnd/routes/signup.js
+++ DiscordCode/BackEnd/routes/signup.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongodb-session')(session);
 
 mongoose.createConnection('mongodb://localhost:27017/colligo');
 
 
 const router = express.Router();
-
+router.use(session({
+    secret: 'secret_key',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
 
 router.post('/', async (req, res) => {
     res.render('signup');
@@ -16,6 +23,13 @@ router.post('/', async (req, res) => {
             password: req.body.password,
         });
         await newUser.save();
+
+        req.session.user = {
+            id: newUser._id,
+            name: newUser.name,
+            email: newUser.email
+        };
+        
         res.status(201).json({message: 'name updated successfully', nname: User.name});
     } catch (err) {
         console.error(err);
