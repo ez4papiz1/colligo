1343e97d44f9b772d4d0dae85ba8e865504b2680
Artem Khachaturov <43856383+ez4papiz1@users.noreply.github.com>
2024-04-08 12:39:28 -0700

    Update login.js
    merged login with the wrong branch

--- DiscordCode/BackEnd/routes/login.js
+++ DiscordCode/BackEnd/routes/login.js
@@ -1,34 +1,45 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const User = require('./Models/Usermodel');
+const MongoStore = require('connect-mongodb-session')(session);
+const Usermodel = require('./Models/Usermodel');
+const bodyParser = require('body-parser');
 
 mongoose.connect("mongodb+srv://Jordan:test123@colligo.jfv09qu.mongodb.net/?retryWrites=true&w=majority&appName=Colligo", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const router = express.Router();
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded());
+
 router.use(session({
     secret: 'secretkey',
     resave: false,
     saveUninitialized: false,
 }));
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     try {
-        User.findOne({ email: email, password: password }).then(user => {
+        Usermodel.findOne({ email: email, password: password} ).then(function(user) {
             req.session.name = user.name;
+            req.session.email = user.email;
             req.session.save();
             console.log('Login successful');
-            res.redirect('/ServerPage')
+            res.redirect('/ServerPage');
         }).catch(error => {
             console.error('Error logging in:', error);
-            res.status(500).send('An error occurred');
+            next(err);
         });
     } catch (err) {
-        console.error(err);
-        res.status(500).send('An error occurred');
+        next(err);
     }
+});
+
+router.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({error: 'Internal Server Error'});
 });
 
 module.exports = router;
