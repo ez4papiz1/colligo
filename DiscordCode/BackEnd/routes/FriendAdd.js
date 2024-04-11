9bd6b21cbc6075f897d5eafad0df778e44af1080
Artem Khachaturov <43856383+ez4papiz1@users.noreply.github.com>
2024-04-11 16:39:59 -0700

    Updated friend add backend

--- DiscordCode/BackEnd/routes/FriendAdd.js
+++ DiscordCode/BackEnd/routes/FriendAdd.js
@@ -18,14 +18,12 @@ router.post('/', async (req, res) => {
         const sendId = req.session.uid;
         const recId = req.body.friendID; 
         console.log(req.body);
-        // Retrieve the user document from the database
         const friend = await User.findOne({ uid: recId });
         console.log(friend);
         if (!friend) {
             return res.status(404).json({error: 'User not found'});
         }
 
-        //Check if request already send
         const sent = await FriendRequests.findOne({ sendId: sendId }, {recId: recId});
         if (sent) {
             return res.status(404).json({error: 'Request already sent'});
@@ -36,15 +34,15 @@ router.post('/', async (req, res) => {
             return res.status(400).json({error: 'Already friend'});
         }
 
-        // Add the friend's ID to the user's friendlist and save the update
         const newReq = new FriendRequests({
             sendid: sendId,
             recid: recId,
-            acc: false,
+            recname: friend.name,
+            sendname: user.name,
         });
         await newReq.save();
        
-        res.status(200).json({message: 'request sent', fname: friend.name});
+        res.redirect('/addFriend');
     } catch (err) {
         console.error(err);
         res.status(500).json({error: 'error'});
